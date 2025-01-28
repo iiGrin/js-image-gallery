095b58d5e3c472adb25dba421c94a6216e1d1263
devIggy <devdevgrin@gmail.com>
2025-01-28 14:10:19 +0300

    feat: clear container in index.html, add dynamic images loading(requestData), json-server

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const slides = document.querySelectorAll('.slide');
-    const currentSlide = document.querySelector('.slide .active');
+    const slidesContainer = document.querySelector('.container');
+    class Slide { // proto for every new img
+        constructor(name, src, alt, parentSelector, ...classes) {
+            this.name = name;
+            this.src = src;
+            this.alt = alt;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+        }
 
-    // for (const slide of slides) {
-    //     slide.addEventListener('click', () => {
-    //         clearActiveClasses();
-    //         slide.classList.add('active');
-    //     });
-    // }
+        render() { // function for image render
+            const renderSlide = document.createElement('div');
+            if (!this.classes.length) { // if classes has not given in constructor
+                this.renderSlide = 'renderSlide';
+                renderSlide.classList.add(this.renderSlide);
+            } else { // if classes has given 
+                this.classes.forEach((className) => {
+                    renderSlide.classList.add(className);
+                });
+            }
+            renderSlide.style.backgroundImage = `url(${this.src})`;
+            renderSlide.innerHTML = `
+                    <h3>${this.name}</h3>
+            `;
+            this.parent.append(renderSlide);
+        }
+    }
 
-    slides.forEach((slide) => {
-        slide.addEventListener('click', () => {
+    const requestData = async (url) => {
+        const response = await fetch(url);
+        return await response.json();
+    };
+
+    requestData('http://localhost:3000/images')
+        .then((data) => {
+            data.forEach((item) => {
+                return new Slide(item.name, item.src, item.alt, '.container', 'slide').render();
+            })
+        });
+
+    slidesContainer.addEventListener('click', (event) => {
+        const slide = event.target.closest('.slide');
+        if (slide) {
             clearActiveClasses();
             slide.classList.add('active');
-        });
+        }
     });
 
     function clearActiveClasses() {
+        const slides = document.querySelectorAll('.slide');
         slides.forEach((slide) => {
             slide.classList.remove('active');
         });
